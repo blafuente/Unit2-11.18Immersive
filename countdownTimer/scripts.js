7a8e6ce70c4a568185d5b66e209ee924cf531992
Brian Lafuente <loulafuente@MacBook-Pro-5.local>
2018-12-10 14:01:42 -0500

    timer

--- countdownTimer/scripts.js
+++ countdownTimer/scripts.js
@@ -8,7 +8,32 @@ function updateTimer(){
     // getTime() can be run against a Date Object, and will convert to the #
     // of the miliseconds since 1-1-1970
     var nowAsTimestamp = now.getTime();
-    console.log(nowAsTimestamp);
+    // console.log(nowAsTimestamp);
+    var timeRemaining = timeStamp - nowAsTimestamp;
+    // console.log(timeRemaining);
+
+    var seconds = Math.floor((timeRemaining / 1000) % 60);
+	var minutes = Math.floor((timeRemaining / 1000 / 60) % 60);
+	var hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+	var days = Math.floor((timeRemaining / (1000 * 60 * 60 * 24)) % 7);
+    var weeks = Math.floor(timeRemaining / (1000 * 60 * 60 * 24 * 7));
+    
+    console.log(weeks,days,hours,minutes,seconds);
+
+    // getElementsByClassName ALWAYS returns an Array, even if there is 0 or 1 thing
+    document.getElementsByClassName('weeks')[0].innerHTML = weeks;
+    // document.getElementsByClassName('days')[0].innerHTML = days;
+    // document.getElementsByClassName('hours')[0].innerHTML = hours;
+    // document.getElementsByClassName('minutes')[0].innerHTML = minutes;
+    // document.getElementsByClassName('seconds')[0].innerHTML = seconds;
+
+    // querySelector will use css rules to find a match,
+    // and only get the first one... NOT an Array
+    document.querySelector('.days').innerHTML = days;
+    document.querySelector('.hours').innerHTML = hours;
+    document.querySelector('.minutes').innerHTML = minutes;
+    document.querySelector('.seconds').innerHTML = seconds;
+
 }
 
 var endDate = new Date("December 25, 2018");
