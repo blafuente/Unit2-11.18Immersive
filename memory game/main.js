fc85988458d422a04d73676d1132642f97a401f7
Brian Lafuente <loulafuente@MacBook-Pro-5.local>
2018-12-17 12:32:19 -0500

    dec 17 b4 lunch

--- memory game/main.js
+++ memory game/main.js
@@ -1,1 +1,13 @@
+// add a ready listener to the whole DOM
+// if., javascript wait until the DOM is finished loading
+// before runnning everything
+$(document).ready(()=>{
+    console.log("sanity check");
 
+    $('button').click(function(){
+        console.log($(this));
+        // after method, will get the value of that attribute
+        let gridSize = $(this).attr('diff');
+        console.log(gridSize);
+    })
+})
